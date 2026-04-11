2b4bf1bcbf2a8843e8c025a8108f0dbd3754adda
Felix Prasse <1330854+flx5@users.noreply.github.com>
2026-04-11 14:11:20 +0200

    Kopia

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from kubernetes import client, config
 
 from backup import Backup, SnapshotInfo, BackupContext
@@ -8,13 +10,21 @@ class BackupDefinition:
     def prepare_snapshots(self, ctx: BackupContext) -> dict[str, SnapshotInfo]:
         pass
 
+def get_current_namespace() -> str:
+    ns_path = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+    if os.path.exists(ns_path):
+        with open(ns_path) as f:
+            return f.read().strip()
+
+    return "default"
+
 def create_backup(definition: BackupDefinition):
     # Configs can be set in Configuration class directly or using helper utility
     configuration = config.load_incluster_config()
 
     # Enter a context with an instance of the API kubernetes.client
     with client.ApiClient(configuration) as api_client:
-        namespace = None #"internal"
+        namespace = get_current_namespace()
         scratch_volume = "backup-scratch"  # TODO Ensure scratch is cleaned?
         application = "nextcloud"
 
