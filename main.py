f997b1424f7c905346899e754a690cae3338cf1f
Felix Prasse <1330854+flx5@users.noreply.github.com>
2026-04-11 14:05:16 +0200

    Kopia

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ class BackupDefinition:
     def prepare_snapshots(self, ctx: BackupContext) -> dict[str, SnapshotInfo]:
         pass
 
-
-
 def create_backup(definition: BackupDefinition):
     # Configs can be set in Configuration class directly or using helper utility
-    configuration = config.load_kube_config()
+    configuration = config.load_incluster_config()
 
     # Enter a context with an instance of the API kubernetes.client
     with client.ApiClient(configuration) as api_client:
